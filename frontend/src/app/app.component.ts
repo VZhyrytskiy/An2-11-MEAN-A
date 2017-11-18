b0335eb798bba5bf67ffe270b9ef572c1a578b7d
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2017-11-18 19:18:01 +0200

    create angular app

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { AppService, Cat } from './app.service';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  cats$: Observable<Cat[]>;
+
+  constructor(
+    private appService: AppService) { }
+
+  ngOnInit() {
+    this.cats$ = this.appService.getAllCats();
+  }
 }
